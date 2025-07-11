a94bb0e246839faa1a47276147ba868fa9d3b838
Adam Wang <adam.wang@hotmail.co.uk>
2025-07-11 12:52:40 +0100

    format files

--- src/converter/converter.ts
+++ src/converter/converter.ts
@@ -7,10 +7,13 @@ import * as vscode from "vscode";
 const model = new ChatOllama({
   baseUrl: "http://localhost:11434",
   model: "llama3",
-  temperature: 0
+  temperature: 0,
 });
 
-async function convertUsingOllama(javaCode: string, outputChannel: vscode.OutputChannel) {
+async function convertUsingOllama(
+  javaCode: string,
+  outputChannel: vscode.OutputChannel,
+) {
   const systemPrompt: string = `
 Translate the given Java code to Kotlin.
 Return only the translated Kotlin code, no extra comments.
@@ -18,25 +21,30 @@ Return only the translated Kotlin code, no extra comments.
 
   const prompt = ChatPromptTemplate.fromMessages([
     ["system", systemPrompt],
-    ["human", [
-      "Java source code we want to translate.\n",
-      "{javaCode}"
-    ].join("")],
+    [
+      "human",
+      ["Java source code we want to translate.\n", "{javaCode}"].join(""),
+    ],
   ]);
 
   const chain = RunnableSequence.from([
     prompt,
     model,
-    (msg: any) => (typeof msg === "string" ? msg : msg.content as string),
+    (msg: any) => (typeof msg === "string" ? msg : (msg.content as string)),
   ]);
 
-  outputChannel.appendLine("convertUsingOllama: Prompt invoked, waiting for response");
+  outputChannel.appendLine(
+    "convertUsingOllama: Prompt invoked, waiting for response",
+  );
   const res = await chain.invoke({ javaCode });
   outputChannel.appendLine("convertUsingOllama: Response received");
 
   return res;
 }
 
-export async function convertToKotlin(javaCode: string, outputChannel: vscode.OutputChannel): Promise<string> {
+export async function convertToKotlin(
+  javaCode: string,
+  outputChannel: vscode.OutputChannel,
+): Promise<string> {
   return await convertUsingOllama(javaCode, outputChannel);
 }

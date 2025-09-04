255df6f5d27c760991385d5fbfb27d45dbfa7e26
Adam Wang <adam.wang@hotmail.co.uk>
2025-09-04 12:51:25 +0100

    implement maven integration

--- extension/src/build-systems/maven.ts
+++ extension/src/build-systems/maven.ts
@@ -20,7 +20,6 @@ export class MavenBuildSystem implements JVMBuildSystem {
       null,
       1,
     );
-
     return file;
   }
 
@@ -42,20 +41,15 @@ export class MavenBuildSystem implements JVMBuildSystem {
 
   async needsKotlin() {
     const uri = await this.getBuildFile();
-    if (!uri) {
-      return false;
-    }
+    if (!uri) return false;
 
     const contents = (await vscode.workspace.openTextDocument(uri)).getText();
-
     return !contents.includes("kotlin-maven-plugin");
   }
 
   async enableKotlin() {
     const uri = await this.getBuildFile();
-    if (!uri) {
-      throw new Error("No pom.xml found in this workspace folder.");
-    }
+    if (!uri) throw new Error("No pom.xml found in this workspace folder.");
 
     const doc = await vscode.workspace.openTextDocument(uri);
     const text = doc.getText();
@@ -73,10 +67,12 @@ export class MavenBuildSystem implements JVMBuildSystem {
     pom.project.properties = pom.project.properties ?? [{}];
     pom.project.dependencies = pom.project.dependencies ?? [{}];
     pom.project.build = pom.project.build ?? [{}];
+
     const props = pom.project.properties[0];
     const depsNode = pom.project.dependencies[0];
     depsNode.dependency = depsNode.dependency ?? [];
 
+    // properties: kotlin.version, kotlin.compiler.jvmTarget
     if (!props["kotlin.version"]) {
       props["kotlin.version"] = [KOTLIN_VERSION];
     }
@@ -85,6 +81,7 @@ export class MavenBuildSystem implements JVMBuildSystem {
       props["kotlin.compiler.jvmTarget"] = [detectedJvmTarget];
     }
 
+    // dependencies
     const deps = depsNode.dependency as any[];
 
     const hasStdlib = deps.some(
@@ -115,81 +112,251 @@ export class MavenBuildSystem implements JVMBuildSystem {
       }
     }
 
+    // --- Build plugins ---
     const build = pom.project.build[0];
     build.plugins = build.plugins ?? [{}];
     build.plugins[0].plugin = build.plugins[0].plugin ?? [];
     const plugins = build.plugins[0].plugin as any[];
 
-    let kotlinPlugin = plugins.find(
-      (p: any) =>
-        p.groupId?.[0] === "org.jetbrains.kotlin" &&
-        p.artifactId?.[0] === "kotlin-maven-plugin",
+    const findPlugin = (gid: string, aid: string) =>
+      plugins.find(
+        (p: any) => p.groupId?.[0] === gid && p.artifactId?.[0] === aid,
+      );
+
+    let kotlinPlugin = findPlugin(
+      "org.jetbrains.kotlin",
+      "kotlin-maven-plugin",
     );
+
+    const ensureArg = (execConf: any, argValue: string) => {
+      execConf.args = execConf.args ?? [{ arg: [] }];
+      const argArr = execConf.args[0].arg as string[];
+      if (!argArr.includes(argValue)) argArr.push(argValue);
+    };
+
+    const ensureCompilerPlugins = (conf: any) => {
+      conf.compilerPlugins = conf.compilerPlugins ?? [{ plugin: [] }];
+      const list = conf.compilerPlugins[0].plugin as string[];
+      const add = (v: string) => {
+        if (!list.includes(v)) list.push(v);
+      };
+      add("spring");
+      add("all-open");
+      add("jpa");
+    };
+
+    const ensurePluginDependency = (plg: any, gid: string, aid: string) => {
+      plg.dependencies = plg.dependencies ?? [{ dependency: [] }];
+      const arr = plg.dependencies[0].dependency as any[];
+      const exists = arr.some(
+        (d: any) => d.groupId?.[0] === gid && d.artifactId?.[0] === aid,
+      );
+      if (!exists) {
+        arr.push({
+          groupId: [gid],
+          artifactId: [aid],
+          version: ["${kotlin.version}"],
+        });
+      }
+    };
 
     if (!kotlinPlugin) {
       kotlinPlugin = {
         groupId: ["org.jetbrains.kotlin"],
         artifactId: ["kotlin-maven-plugin"],
         version: ["${kotlin.version}"],
+        configuration: [
+          {
+            jvmTarget: ["${kotlin.compiler.jvmTarget}"],
+          },
+        ],
         executions: [
           {
             execution: [
               {
-                id: ["kotlin-compile"],
-                phase: ["process-sources"],
+                id: ["compile-kotlin"],
+                phase: ["compile"],
                 goals: [{ goal: ["compile"] }],
+                configuration: [
+                  {
+                    args: [{ arg: [`-Xjava-source-roots=\${project.basedir}/src/main/java`] }],
+                  },
+                ],
               },
               {
-                id: ["kotlin-test-compile"],
-                phase: ["process-test-sources"],
+                id: ["test-compile-kotlin"],
+                phase: ["test-compile"],
                 goals: [{ goal: ["test-compile"] }],
+                configuration: [
+                  {
+                    args: [{ arg: [`-Xjava-source-roots=\${project.basedir}/src/test/java`] }],
+                  },
+                ],
               },
             ],
-          },
-        ],
-        configuration: [
-          {
-            jvmTarget: ["${kotlin.compiler.jvmTarget}"],
           },
         ],
       };
 
       if (configureInPlaceReplacements) {
         kotlinPlugin.configuration[0].sourceDirs = [
-          {
-            source: ["src/main/kotlin", "src/main/java"],
-          },
+          { source: ["src/main/java", "src/main/kotlin"] },
         ];
         kotlinPlugin.configuration[0].testSourceDirs = [
-          {
-            source: ["src/test/kotlin", "src/test/java"],
-          },
+          { source: ["src/test/java", "src/test/kotlin"] },
         ];
       }
+      ensureCompilerPlugins(kotlinPlugin.configuration[0]);
+
+      ensurePluginDependency(
+        kotlinPlugin,
+        "org.jetbrains.kotlin",
+        "kotlin-maven-allopen",
+      );
+      ensurePluginDependency(
+        kotlinPlugin,
+        "org.jetbrains.kotlin",
+        "kotlin-maven-noarg",
+      );
 
       plugins.push(kotlinPlugin);
     } else {
+      kotlinPlugin.version = kotlinPlugin.version ?? ["${kotlin.version}"];
       kotlinPlugin.configuration = kotlinPlugin.configuration ?? [{}];
       const conf = kotlinPlugin.configuration[0];
-      if (!conf.jvmTarget) {
-        conf.jvmTarget = ["${kotlin.compiler.jvmTarget}"];
-      }
 
+      if (!conf.jvmTarget) conf.jvmTarget = ["${kotlin.compiler.jvmTarget}"];
       if (configureInPlaceReplacements) {
         conf.sourceDirs = conf.sourceDirs ?? [{ source: [] }];
         conf.testSourceDirs = conf.testSourceDirs ?? [{ source: [] }];
         const src = conf.sourceDirs[0].source as string[];
         const testSrc = conf.testSourceDirs[0].source as string[];
         const addIfMissing = (arr: string[], v: string) => {
-          if (!arr.includes(v)) {
-            arr.push(v);
-          }
+          if (!arr.includes(v)) arr.push(v);
         };
-        addIfMissing(src, "src/main/kotlin");
         addIfMissing(src, "src/main/java");
-        addIfMissing(testSrc, "src/test/kotlin");
+        addIfMissing(src, "src/main/kotlin");
         addIfMissing(testSrc, "src/test/java");
+        addIfMissing(testSrc, "src/test/kotlin");
       }
+      ensureCompilerPlugins(conf);
+
+      kotlinPlugin.executions = kotlinPlugin.executions ?? [{ execution: [] }];
+      const execs = kotlinPlugin.executions[0].execution as any[];
+
+      const upsertExec = (id: string, phase: string, javaRootsArg: string) => {
+        let e = execs.find((x: any) => (x.id?.[0] ?? "") === id);
+        if (!e) {
+          e = {
+            id: [id],
+            phase: [phase],
+            goals: [{ goal: [id.startsWith("test-") ? "test-compile" : "compile"] }],
+            configuration: [{}],
+          };
+          execs.push(e);
+        } else {
+          e.phase = [phase];
+          e.goals = [{ goal: [id.startsWith("test-") ? "test-compile" : "compile"] }];
+          e.configuration = e.configuration ?? [{}];
+        }
+        const econf = e.configuration[0];
+        ensureArg(econf, javaRootsArg);
+      };
+
+      upsertExec(
+        "compile-kotlin",
+        "compile",
+        "-Xjava-source-roots=${project.basedir}/src/main/java",
+      );
+      upsertExec(
+        "test-compile-kotlin",
+        "test-compile",
+        "-Xjava-source-roots=${project.basedir}/src/test/java",
+      );
+
+      ensurePluginDependency(
+        kotlinPlugin,
+        "org.jetbrains.kotlin",
+        "kotlin-maven-allopen",
+      );
+      ensurePluginDependency(
+        kotlinPlugin,
+        "org.jetbrains.kotlin",
+        "kotlin-maven-noarg",
+      );
+    }
+
+    let mavenCompiler = findPlugin(
+      "org.apache.maven.plugins",
+      "maven-compiler-plugin",
+    );
+    if (!mavenCompiler) {
+      mavenCompiler = {
+        groupId: ["org.apache.maven.plugins"],
+        artifactId: ["maven-compiler-plugin"],
+        version: ["3.14.0"],
+        executions: [
+          {
+            execution: [
+              {
+                id: ["default-compile"],
+                goals: [{ goal: ["compile"] }],
+                configuration: [{ skipMain: ["true"] }],
+              },
+              {
+                id: ["compile-java"],
+                phase: ["compile"],
+                goals: [{ goal: ["compile"] }],
+                configuration: [{ release: ["${java.version}"] }],
+              },
+              {
+                id: ["default-testCompile"],
+                goals: [{ goal: ["testCompile"] }],
+                configuration: [{ skip: ["true"] }],
+              },
+              {
+                id: ["test-compile-java"],
+                phase: ["test-compile"],
+                goals: [{ goal: ["testCompile"] }],
+                configuration: [{ release: ["${java.version}"] }],
+              },
+            ],
+          },
+        ],
+      };
+      plugins.push(mavenCompiler);
+    } else {
+      mavenCompiler.version = mavenCompiler.version ?? ["3.14.0"];
+      mavenCompiler.executions = mavenCompiler.executions ?? [{ execution: [] }];
+      const execs = mavenCompiler.executions[0].execution as any[];
+
+      const setExec = (id: string, update: any) => {
+        let e = execs.find((x: any) => (x.id?.[0] ?? "") === id);
+        if (!e) {
+          e = { id: [id] };
+          execs.push(e);
+        }
+        Object.assign(e, update);
+      };
+
+      setExec("default-compile", {
+        goals: [{ goal: ["compile"] }],
+        configuration: [{ skipMain: ["true"] }],
+      });
+      setExec("compile-java", {
+        phase: ["compile"],
+        goals: [{ goal: ["compile"] }],
+        configuration: [{ release: ["${java.version}"] }],
+      });
+      setExec("default-testCompile", {
+        goals: [{ goal: ["testCompile"] }],
+        configuration: [{ skip: ["true"] }],
+      });
+      setExec("test-compile-java", {
+        phase: ["test-compile"],
+        goals: [{ goal: ["testCompile"] }],
+        configuration: [{ release: ["${java.version}"] }],
+      });
     }
 
     const updated = builder.buildObject(pom);

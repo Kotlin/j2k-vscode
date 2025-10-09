b565fca7328b40b2459015436c8ad8466b259f78
Adam Wang <adam.wang@hotmail.co.uk>
2025-10-09 21:18:43 +0100

    add extra prompt structure for spring app

---
+++ extension/src/converter/prompt/specific/lombok.ts
@@ -0,0 +1,1 @@
+export const LOMBOK_PROMPT = "Do not convert Lombok: remove Lombok annotations entirely. Convert these annotations idiomatically into their exact Kotlin counterparts.";
---
+++ extension/src/converter/prompt/specific/spring.ts
@@ -0,0 +1,1 @@
+export const SPRING_PROMPT = "When running a Spring app, make sure the command line arguments are spread in Kotlin.";

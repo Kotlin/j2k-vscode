c2ac164bc2add6997b61779e2a322a463381f082
Adam Wang <adam.wang@hotmail.co.uk>
2026-03-06 01:37:46 +0000

    factor out prompt from extension

--- extension/src/converter/prompt/specific/hibernate.ts
+++ extension/src/converter/prompt/specific/hibernate.ts
@@ -1,2 +1,3 @@
-export const HIBERNATE_PROMPT =
-  "For any JPA-annotated entities, do not convert these into Kotlin data-classes. Keep entity classes open so that Hibernate can create proxies. If Hibernate requires, provide a no-argument constructor as well.";
+import hibernate from "../../../../../prompt/specific/hibernate.txt";
+
+export const HIBERNATE_PROMPT = hibernate;
